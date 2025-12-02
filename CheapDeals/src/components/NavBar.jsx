e118a1f457aaa3e4dbe63beff22769f8d69a83fb
Miguel <gonzalez98m@gmail.com>
2025-12-01 17:22:44 -0800

    change name NavBar

--- CheapDeals/src/components/NavBar.jsx
+++ CheapDeals/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css";
 function NavBar () {
     return <nav className="navbar"> 
         <div className="navbar-brand">
-            <Link to="/">Cheap Deals</Link>
+            <Link to="/">Speed Deals</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
